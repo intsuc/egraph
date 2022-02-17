175cf1b6d82f944d4f6dc63ed9a197e3c64e491e
intsuc <intsuc@gmail.com>
2022-02-18 06:49:46 +0900

    Use getter and setter

--- src/main/kotlin/egraph/UnionFind.kt
+++ src/main/kotlin/egraph/UnionFind.kt
@@ -7,6 +7,18 @@ class UnionFind(capacity: Int = 10) {
     private val parents: IntArrayList = IntArrayList(capacity)
     private val ranks: ByteArrayList = ByteArrayList(capacity)
 
+    private inline var Int.parent
+        get(): Int = parents.getInt(this)
+        set(value) {
+            parents[this] = value
+        }
+
+    private inline var Int.rank
+        get(): Byte = ranks.getByte(this)
+        set(value) {
+            ranks[this] = value
+        }
+
     fun new(): Int = parents.size.also {
         parents += it
         ranks += 0
@@ -18,24 +30,24 @@ class UnionFind(capacity: Int = 10) {
 
         if (root1 == root2) return
 
-        val rank1 = ranks.getByte(root1)
-        val rank2 = ranks.getByte(root2)
+        val rank1 = root1.rank
+        val rank2 = root2.rank
 
         if (rank1 < rank2) {
-            parents[root1] = root2
+            root1.parent = root2
         } else if (rank1 > rank2) {
-            parents[root2] = root1
+            root2.parent = root1
         } else {
-            parents[root2] = root1
-            ranks[root1] = (ranks.getByte(root1) + 1).toByte()
+            root2.parent = root1
+            root1.rank = root1.rank.inc()
         }
     }
 
     fun find(x: Int): Int {
         var self = x
-        while (self != parents.getInt(self)) {
-            val grandparent = parents.getInt(parents.getInt(self))
-            parents[self] = grandparent
+        while (self != self.parent) {
+            val grandparent = self.parent.parent
+            self.parent = grandparent
             self = grandparent
         }
         return self
